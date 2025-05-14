9ad215458da1380fd11732a9cc36a00e74ded68c
isaac-rom3ro <is4ac.romero@outlook.com>
2025-05-14 10:52:50 -0300

    qrcode working

--- script/generate-qrcode.js
+++ script/generate-qrcode.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(){
     const container = document.getElementById("qrcode")
 
-        
+        const destinyUrl = window.location.origin + window.location.pathname.replace("final-decryption.html", "")+ "Caiu-no-golpe.html";
 
         new QRCode(container, {
-            text: "Hello World!", 
+            text: destinyUrl, 
             colorDark: "#000000",
             colorLight: "#ffffff",
             correctLevel: QRCode.CorrectLevel.H
